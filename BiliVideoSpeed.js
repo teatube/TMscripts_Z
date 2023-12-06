7cb5a4ccfa1e1f1012d725060cc297b20ef73a66
MrZ_26 <1046101471@qq.com>
2023-12-07 05:19:54 +0800

    代码格式化

--- BiliVideoSpeed.js
+++ BiliVideoSpeed.js
@@ -10,13 +10,11 @@
 (function() {
     var video = document.getElementsByTagName("video")[0];
 
-    function keyd(e){
-        if(e.key == "=" || e.key == "-"){
+    function keyd(e) {
+        if (e.key == "=" || e.key == "-") {
             var speed = video.playbackRate;
-            if(e.key == "=")
-                speed = speed < 1 ? speed + 0.1 : speed + 0.25;
-            else
-                speed = speed <= 1 ? speed - 0.1 : speed - 0.25;
+            if (e.key == "=") speed = speed < 1 ? speed + 0.1 : speed + 0.25;
+            else speed = speed <= 1 ? speed - 0.1 : speed - 0.25;
             video.playbackRate = speed.toFixed(2);
 
             var speedLabel = document.getElementsByClassName('bilibili-player-video-btn-speed-name')[0];
